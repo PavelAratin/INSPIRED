1e1b6de2dc9778027d04f8348bdc1c45aee976d0
Pavel <paha_gagarin@mail.ru>
2023-01-14 11:13:18 +0300

    вывод верстки в секции херо в завсимости от гендера

--- src/modules/mainPage/mainPage.js
+++ src/modules/mainPage/mainPage.js
@@ -4,7 +4,6 @@ import { renderProducts } from "../render/renderProducts";
 //вывод отдельных частей верстки на страницу
 
 export const mainPage = (gender = 'women') => {
-  console.log(gender);
   renderNavigation(gender);
   renderHero(gender);
   renderProducts(gender);
--- src/modules/render/renderHero.js
+++ src/modules/render/renderHero.js
@@ -5,12 +5,12 @@ export const renderHero = (gender) => {
     return
   }
   hero.style.display = '';
-  hero.className = `hero ${hero__gender}`;
+  hero.className = `hero hero__${gender}`;
   hero.innerHTML = `
  <div class="container">
-        <div class="hero__content">
-          <h1 class="title hero__title">Новая коллекция бюстгальтер-балконет</h1>
-          <a href="#" class="hero__link">Перейти</a>
-        </div>
-      </div>`;
+    <div class="hero__content">
+      <h1 class="title hero__title">${gender === 'women'? 'Новая коллекция бюстгальтер-балконет':'Боксеры из новой кооллекции'}</h1>
+      <a href="#" class="hero__link">Перейти</a>
+    </div>
+  </div>`;
 };
