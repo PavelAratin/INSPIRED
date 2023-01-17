5c211c06caf616143113d39ef95036f81a4b882f
Pavel <paha_gagarin@mail.ru>
2023-01-17 11:43:03 +0300

    обработка ошибки при первом запуске , если сервер не работает

--- src/index.js
+++ src/index.js
@@ -13,34 +13,49 @@ import { getData } from "./modules/getData";
 import { API_URL } from "./modules/const";
 import { DATA } from "./modules/const";
 import { createCssColors } from "./modules/creareCssColors";
+import { createElement } from "./modules/createElement";
 
 //обращение к серверу
 const init = async () => {
-  DATA.navigation = await getData(`${API_URL}/api/categories`);
-  DATA.colors = await getData(`${API_URL}/api/colors`);
-  //функция для создания динамического css на основе данных с сервера
-  createCssColors(DATA.colors)
-  //через библиотеку
-  router.on('*', () => {
-    renderHeader();
-    renderFooter();
-  })
-  //через библиотеку
-  router.on('/', () => {
-    //вывод навигации,херо,товаров на главную страницу
-    mainPage();
-  })
-  //через библиотеку
-  router.on('women', () => {
-    //вывод навигации,херо,товаров на главную страницу
-    womenMainPage();
-  })
-  //через библиотеку
-  router.on('men', () => {
-    //вывод навигации,херо,товаров на главную страницу
-    menMainPage();
-  })
-  router.resolve();
+  try {
+    //через библиотеку
+    router.on('*', () => {
+      renderHeader();
+      renderFooter();
+    })
+    DATA.navigation = await getData(`${API_URL}/api/categories`);
+    DATA.colors = await getData(`${API_URL}/api/colors`);
+    //функция для создания динамического css на основе данных с сервера
+    createCssColors(DATA.colors)
+    //через библиотеку
+    router.on('/', () => {
+      //вывод навигации,херо,товаров на главную страницу
+      mainPage();
+    })
+    //через библиотеку
+    router.on('women', () => {
+      //вывод навигации,херо,товаров на главную страницу
+      womenMainPage();
+    })
+    //через библиотеку
+    router.on('men', () => {
+      //вывод навигации,херо,товаров на главную страницу
+      menMainPage();
+    })
+    
+  } catch (error) {
+    createElement('h2',{
+      textContent:'Что-то пошло не так, попробуйте позже '
+    },{
+      parent:document.querySelector('section'),
+      cb(h2){
+        h2.style.textAlign='center'
+      }
+    })
+    
+  } finally {
+    router.resolve();
+  }
 }
 init()
 
